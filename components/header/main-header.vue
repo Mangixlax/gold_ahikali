c3bf5844bfe900a8acfc8d1ef7a4fb6837cfdea5
Джангетов Аслан <a.djangetov@specavia.loc>
2020-11-20 17:58:55 +0300

    created project

---
+++ components/header/main-header.vue
@@ -0,0 +1,6 @@
+<template>
+	<div>
+		<topline-header/>
+		<botline-header/>	
+	</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <main-header/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
